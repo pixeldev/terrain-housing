bc95529aa24ddcc569d3f633fcfdf056fa0a3a9c
pixeldev <pixeldev1729@gmail.com>
2022-01-29 13:09:43 -0600

    deps: add dependency injection

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,16 @@ plugins {
 
 repositories {
     mavenLocal()
+    maven("https://repo.unnamed.team/repository/unnamed-public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     mavenCentral()
 }
 
 dependencies {
     implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.jetbrains:annotations:22.0.0")
+    implementation("me.yushust.inject:core:0.4.5-SNAPSHOT")
+
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.0.1") { isTransitive = false }
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.0.1") { isTransitive = false }
