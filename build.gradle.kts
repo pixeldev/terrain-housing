b693f2d76d00416355a5416d0c4423f8d7b193b4
pixeldev <pixeldev1729@gmail.com>
2022-01-29 13:00:15 -0600

    deps: add HikariCP

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.zaxxer:HikariCP:5.0.1")
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.0.1") { isTransitive = false }
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.0.1") { isTransitive = false }
