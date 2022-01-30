6919001ebf6bbfb35e772108e31a1f8056e7cdb9
pixeldev <pixeldev1729@gmail.com>
2022-01-30 09:49:01 -0600

    feat(message): implement a method to handle list of messages

--- src/main/java/net/cosmogrp/thousing/message/MessageHandler.java
+++ src/main/java/net/cosmogrp/thousing/message/MessageHandler.java
@@ -5,6 +5,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 import javax.inject.Inject;
 
+import java.util.List;
+
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
 public class MessageHandler {
@@ -18,6 +20,22 @@ public class MessageHandler {
         sender.sendMessage(makeMessage(path, replacements));
     }
 
+    public void sendMessages(
+            CommandSender sender,
+            String path, String... replacements
+    ) {
+        sender.sendMessage(String.join(
+                "\n",
+                makeMessages(path, replacements)
+        ));
+    }
+
+    public List<String> makeMessages(String path, String... replacements) {
+        List<String> messages = configuration.getStringList("messages." + path);
+        messages.replaceAll(line -> applyReplacements(line, replacements));
+        return messages;
+    }
+
     public String makeMessage(String path, String... replacements) {
         String message = configuration.getString("messages." + path);
 
@@ -25,6 +43,10 @@ public class MessageHandler {
             return path;
         }
 
+        return applyReplacements(message, replacements);
+    }
+
+    private String applyReplacements(String message, String... replacements) {
         for (int i = 0 ; i < replacements.length ; i += 2) {
             message = message.replace(
                     replacements[i],
