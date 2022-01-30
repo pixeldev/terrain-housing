3f105eaf3f71170788464c7acf08d55aabf3b608
pixeldev <pixeldev1729@gmail.com>
2022-01-30 12:36:42 -0600

    feat(terrain): implement claimed terrain as codec

--- src/main/java/net/cosmogrp/thousing/terrain/ClaimedTerrain.java
+++ src/main/java/net/cosmogrp/thousing/terrain/ClaimedTerrain.java
@@ -1,20 +1,31 @@
 package net.cosmogrp.thousing.terrain;
 
+import net.cosmogrp.thousing.codec.Codec;
+import net.cosmogrp.thousing.util.DataStreams;
 import org.bukkit.entity.Player;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-public class ClaimedTerrain {
+public class ClaimedTerrain implements Codec {
 
     private String terrainId;
 
-    private final UUID ownerId;
+    private UUID ownerId;
     private final Set<UUID> authorizedPlayers;
 
-    public ClaimedTerrain(UUID ownerId) {
+    private ClaimedTerrain(String terrainId, UUID ownerId) {
+        this.terrainId = terrainId;
         this.ownerId = ownerId;
+        this.authorizedPlayers = new HashSet<>();
+    }
+
+    private ClaimedTerrain() {
+        // For codec
         this.authorizedPlayers = new HashSet<>();
     }
 
@@ -24,10 +35,6 @@ public class ClaimedTerrain {
 
     public UUID getOwnerId() {
         return ownerId;
-    }
-
-    public void setTerrainId(String terrainId) {
-        this.terrainId = terrainId;
     }
 
     public boolean authorizePlayer(Player player) {
@@ -42,4 +49,33 @@ public class ClaimedTerrain {
         return authorizedPlayers.contains(player.getUniqueId());
     }
 
+    public static ClaimedTerrain from(String terrainId, UUID ownerId) {
+        return new ClaimedTerrain(terrainId, ownerId);
+    }
+
+    public static ClaimedTerrain from(DataInputStream in) throws IOException {
+        ClaimedTerrain claimedTerrain = new ClaimedTerrain();
+        claimedTerrain.read(in);
+        return claimedTerrain;
+    }
+
+    @Override
+    public void write(DataOutputStream output) throws IOException {
+        DataStreams.writeString(terrainId, output);
+        DataStreams.writeUuid(ownerId, output);
+        output.writeInt(authorizedPlayers.size());
+        for (UUID uuid : authorizedPlayers) {
+            DataStreams.writeUuid(uuid, output);
+        }
+    }
+
+    @Override
+    public void read(DataInputStream input) throws IOException {
+        terrainId = DataStreams.readString(input);
+        ownerId = DataStreams.readUuid(input);
+        int size = input.readInt();
+        for (int i = 0; i < size; i++) {
+            authorizedPlayers.add(DataStreams.readUuid(input));
+        }
+    }
 }
