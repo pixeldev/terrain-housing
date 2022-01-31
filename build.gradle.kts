9f07d601a5ef6484e8e8004a70f3e28cab28b97f
pixeldev <pixeldev1729@gmail.com>
2022-01-31 13:10:48 -0600

    deps: add placeholder api

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
     maven("https://repo.unnamed.team/repository/unnamed-public/")
     maven("https://maven.enginehub.org/repo/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     mavenCentral()
 }
 
@@ -16,6 +17,7 @@ dependencies {
     implementation("me.fixeddev:commandflow-brigadier:0.5.0-SNAPSHOT")
     implementation("me.yushust.inject:core:0.4.5-SNAPSHOT")
 
+    compileOnly("me.clip:placeholderapi:2.11.1")
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.6")
